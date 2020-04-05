e7cfd1ed1e1f7144b952d563acd64d3e89fa89a7
Tyler Marrs <tylerwmarrs@gmail.com>
2020-04-05 07:10:53 -0500

    [skip ci] update deploy docs script to use github pages instead of personal VPS

--- docs/deploy_docs.sh
+++ docs/deploy_docs.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
+set -e
 
-rsync -rav -e ssh --delete build/html/ matrixprofile.docs.matrixprofile.org:/home/tyler/www/matrixprofile.docs.matrixprofile.org/www
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+HTMLDIR="${SCRIPTPATH}/build/html"
+MSG="deployed docs"
+
+while getopts m: option
+do
+    case "${option}"
+    in
+        m) MSG=${OPTARG};;
+    esac
+done
+
+make html
+cd /tmp/
+
+if [ -d "matrixprofile-docs-website" ]; then
+    rm -rf matrixprofile-docs-website
+fi
+
+
+git clone git@github.com:matrix-profile-foundation/matrixprofile-docs-website.git
+cd matrixprofile-docs-website
+git checkout master
+git rm -rf .
+git clean -fxd
+echo "matrixprofile.docs.matrixprofile.org" > CNAME
+cp -R "${HTMLDIR}"/* .
+git add .
+git commit -am "${MSG}"
+git push origin master
+cd /
+rm -rf /tmp/matrixprofile-docs-website
